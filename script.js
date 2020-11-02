f24011cbbdeea1c989b2e533f56e646cbc6c3839
Jonathan Deschuyteneer <deschuyteneerj@gmail.com>
2020-11-02 10:17:30 +0100

    update

---
+++ script.js
@@ -0,0 +1,711 @@
+//Testing array with special characters:
+// let words = ["rendez-vous",
+//         "s'il-vous-plait"]
+
+let words = [
+    "accommodation",
+    "accomplished",
+    "accuracy",
+    "achievement",
+    "adjustment",
+    "administrator",
+    "adult",
+    "advance",
+    "advancement",
+    "affair",
+    "affection",
+    "affectionate",
+    "aging",
+    "agony",
+    "alert",
+    "alone",
+    "altar",
+    "alternative",
+    "amateur",
+    "ambassador",
+    "ambitious",
+    "analysis",
+    "angel",
+    "angry",
+    "animation",
+    "antenna",
+    "antique",
+    "anxiety",
+    "appearance",
+    "appropriate",
+    "architecture",
+    "arena",
+    "arsenal",
+    "artificial",
+    "asset",
+    "assistant",
+    "associate",
+    "asylum",
+    "athletic",
+    "attached",
+    "attitude",
+    "authentic",
+    "autumn",
+    "awful",
+    "awkward",
+    "backup",
+    "baggage",
+    "balloon",
+    "bankrupt",
+    "baseball",
+    "battle",
+    "beast",
+    "beforehand",
+    "beverage",
+    "bicycle",
+    "biology",
+    "birth",
+    "blank",
+    "blessing",
+    "bonus",
+    "bought",
+    "bound",
+    "boundary",
+    "bowling",
+    "boxing",
+    "brand",
+    "brave",
+    "breakdown",
+    "breaking",
+    "bright",
+    "brightness",
+    "brink",
+    "bronze",
+    "browser",
+    "brute",
+    "budget",
+    "building",
+    "bully",
+    "burning",
+    "cabbage",
+    "calculator",
+    "campus",
+    "candidate",
+    "canvas",
+    "casting",
+    "category",
+    "century",
+    "certification",
+    "championship",
+    "chemical",
+    "chess",
+    "chosen",
+    "church",
+    "civil",
+    "civilian",
+    "civility",
+    "classification",
+    "climate",
+    "closet",
+    "clown",
+    "collect",
+    "collector",
+    "color",
+    "combat",
+    "comedian",
+    "comic",
+    "commentary",
+    "compact",
+    "compass",
+    "complaint",
+    "completion",
+    "complex",
+    "complexity",
+    "computer",
+    "confession",
+    "confusion",
+    "connected",
+    "consumption",
+    "content",
+    "controller",
+    "conversation",
+    "converter",
+    "copper",
+    "corpse",
+    "corrupt",
+    "covert",
+    "creature",
+    "criminal",
+    "crimson",
+    "criticism",
+    "crown",
+    "crushed",
+    "crystal",
+    "cultural",
+    "curiosity",
+    "daily",
+    "daughter",
+    "decade",
+    "declaration",
+    "deliver",
+    "density",
+    "depression",
+    "description",
+    "desirable",
+    "desktop",
+    "destruction",
+    "determination",
+    "determined",
+    "dictator",
+    "dictionary",
+    "dinner",
+    "diplomacy",
+    "director",
+    "discovery",
+    "education",
+    "efficiency",
+    "election",
+    "elevator",
+    "english",
+    "enemy",
+    "envoy",
+    "equipment",
+    "exception",
+    "excitement",
+    "existence",
+    "extra",
+    "famous",
+    "favorite",
+    "fearful",
+    "fiction",
+    "fighter",
+    "finished",
+    "finnish",
+    "football",
+    "footprint",
+    "forced",
+    "forehead",
+    "foreign",
+    "fossil",
+    "freedom",
+    "friendship",
+    "frozen",
+    "frustration",
+    "function",
+    "funny",
+    "futile",
+    "generation",
+    "generator",
+    "generous",
+    "government",
+    "graduated",
+    "graphics",
+    "grief",
+    "growing",
+    "guest",
+    "guidance",
+    "hangman",
+    "hardship",
+    "harmony",
+    "heart",
+    "height",
+    "hierarchy",
+    "history",
+    "hobby",
+    "holiday",
+    "honest",
+    "house",
+    "humanity",
+    "hungry",
+    "hunting",
+    "ideology",
+    "illusion",
+    "illustration",
+    "imagination",
+    "immigrant",
+    "important",
+    "impossible",
+    "improvement",
+    "income",
+    "innocent",
+    "innovation",
+    "insanity",
+    "insect",
+    "inspiration",
+    "instruction",
+    "interrupt",
+    "invasion",
+    "invitation",
+    "island",
+    "journalism",
+    "keyboard",
+    "knitting",
+    "labour",
+    "landscape",
+    "laptop",
+    "laundry",
+    "leadership",
+    "legal",
+    "legendary",
+    "library",
+    "lighting",
+    "lightning",
+    "limit",
+    "limitation",
+    "loose",
+    "luxury",
+    "lying",
+    "machinery",
+    "magazine",
+    "magic",
+    "magnificent",
+    "makeup",
+    "mandatory",
+    "manual",
+    "marked",
+    "marketplace",
+    "master",
+    "masterpiece",
+    "mathematics",
+    "media",
+    "migration",
+    "mining",
+    "mistaken",
+    "mixture",
+    "moderator",
+    "modern",
+    "modest",
+    "monthly",
+    "monument",
+    "morning",
+    "mouse",
+    "mountain",
+    "music",
+    "musical",
+    "mythology",
+    "natural",
+    "navigator",
+    "nephew",
+    "neutrality",
+    "noisy",
+    "north",
+    "northeast",
+    "northwest",
+    "notebook",
+    "number",
+    "nutrition",
+    "ocean",
+    "olive",
+    "onion",
+    "opening",
+    "opponent",
+    "orange",
+    "organic",
+    "organization",
+    "organizer",
+    "oversight",
+    "overweight",
+    "packaging",
+    "padding",
+    "painting",
+    "paranoid",
+    "partisan",
+    "party",
+    "peaceful",
+    "pencil",
+    "percentage",
+    "performance",
+    "personal",
+    "personality",
+    "perspective",
+    "philosophy",
+    "photography",
+    "piracy",
+    "plausible",
+    "playground",
+    "pleasant",
+    "pocket",
+    "polished",
+    "polite",
+    "politician",
+    "popularity",
+    "portfolio",
+    "poverty",
+    "power",
+    "practical",
+    "prayer",
+    "precious",
+    "presentation",
+    "pretty",
+    "private",
+    "professor",
+    "progression",
+    "promise",
+    "proof",
+    "propaganda",
+    "protective",
+    "prototype",
+    "puppet",
+    "purple",
+    "quiet",
+    "railway",
+    "ratio",
+    "reader",
+    "reasoning",
+    "rebel",
+    "rebellion",
+    "recession",
+    "recipe",
+    "recognition",
+    "refrigerator",
+    "registration",
+    "relief",
+    "remarkable",
+    "remorse",
+    "repetition",
+    "reputation",
+    "resemblance",
+    "resolution",
+    "resource",
+    "retirement",
+    "river",
+    "romance",
+    "rotten",
+    "royalty",
+    "running",
+    "sadness",
+    "salary",
+    "satellite",
+    "sausage",
+    "scattered",
+    "school",
+    "security",
+    "select",
+    "selective",
+    "selfish",
+    "senior",
+    "sensation",
+    "sensitive",
+    "series",
+    "serving",
+    "session",
+    "severity",
+    "shadow",
+    "sheep",
+    "sheet",
+    "sheriff",
+    "shipment",
+    "shocking",
+    "short",
+    "shotgun",
+    "silver",
+    "skate",
+    "skeleton",
+    "skill",
+    "sleeping",
+    "social",
+    "solitary",
+    "solitude",
+    "sophisticated",
+    "sorrow",
+    "south",
+    "southeast",
+    "southwest",
+    "speaker",
+    "spelling",
+    "spider",
+    "sport",
+    "standard",
+    "state",
+    "status",
+    "statue",
+    "steam",
+    "steep",
+    "stereo",
+    "stone",
+    "storage",
+    "store",
+    "stormy",
+    "straw",
+    "strong",
+    "stubborn",
+    "stuck",
+    "student",
+    "succession",
+    "suffering",
+    "summary",
+    "summer",
+    "sunset",
+    "superintendent",
+    "surgery",
+    "suspicious",
+    "Swedish",
+    "tactical",
+    "tasty",
+    "taxation",
+    "technique",
+    "technology",
+    "template",
+    "terminal",
+    "termination",
+    "terrible",
+    "territory",
+    "terror",
+    "theater",
+    "theory",
+    "thief",
+    "threatening",
+    "throat",
+    "tired",
+    "tolerance",
+    "topping",
+    "total",
+    "tracker",
+    "traitor",
+    "transparent",
+    "treasury",
+    "treatment",
+    "trust",
+    "tuition",
+    "tutorial",
+    "umbrella",
+    "underground",
+    "understanding",
+    "underwater",
+    "universe",
+    "university",
+    "unstable",
+    "usage",
+    "utility",
+    "vaccine",
+    "variety",
+    "vegetarian",
+    "veteran",
+    "viable",
+    "vicinity",
+    "video",
+    "villain",
+    "vintage",
+    "violent",
+    "virus",
+    "visibility",
+    "volatile",
+    "volcano",
+    "wardrobe",
+    "warfare",
+    "warning",
+    "warrior",
+    "weekly",
+    "whole",
+    "wilderness",
+    "wooden",
+    "world",
+    "worthless",
+    "wound",
+    "wretched",
+    "writer",
+    "writing",
+    "wrong",
+    "young"
+]
+
+//Array used for the split word:
+let arrayRandomWordSplit = [];
+//Create an empty array to display "_" as many times as there is characters in the word to guess in the arrayRandomWord:
+let arrayWord_ = [];
+//Create stats of the game:
+let numberOfTries = 10;
+//Create an array of letters that fit or does not fit in the word to guess:
+let arrayRightLetters = [];
+let arrayWrongLetters = [];
+//Create an array without duplicate of wrong letters:
+let uniqueWrong = [];
+let uniqueRight = [];
+console.log(uniqueWrong);
+
+//Function to replace special characters by normal characters:
+accentsTidy = function(s){
+    var r=s.toLowerCase();
+    //r = r.replace(new RegExp(/\s/g),"");
+    r = r.replace(new RegExp(/[àáâãäå]/g),"a");
+    r = r.replace(new RegExp(/æ/g),"ae");
+    r = r.replace(new RegExp(/ç/g),"c");
+    r = r.replace(new RegExp(/[èéêë]/g),"e");
+    r = r.replace(new RegExp(/[ìíîï]/g),"i");
+    r = r.replace(new RegExp(/ñ/g),"n");                
+    r = r.replace(new RegExp(/[òóôõö]/g),"o");
+    r = r.replace(new RegExp(/œ/g),"oe");
+    r = r.replace(new RegExp(/[ùúûü]/g),"u");
+    r = r.replace(new RegExp(/[ýÿ]/g),"y");
+    //r = r.replace(new RegExp(/\W/g),"");
+    return r;
+};
+
+//Choose a random word inside of the words array:
+let randomWord = words[Math.floor(Math.random() * words.length)];
+randomWord = accentsTidy(randomWord);
+
+//Create a var to stock number of special characters inside the word:
+let stockNonWordCharacter = randomWord.match(/\W/) || [];
+console.log(stockNonWordCharacter);
+console.log(stockNonWordCharacter.length);
+
+//Console.log to check the random word chosen:
+//console.log("random word =>", randomWord);
+
+//Display the word chosen into a <p>:
+document.getElementById("randomWordGuess").innerHTML = randomWord;
+
+//Split the word into an array of letters:
+for (let i = 0; i < randomWord.length; i++) {
+    arrayRandomWordSplit.push(randomWord.charAt(i));
+}
+
+//Console.log to check the array of letters of the random word:
+//console.log(arrayRandomWordSplit);
+
+//Check if the letter to replace is a letter or a special character, if it is a special character, does not replace it:
+arrayRandomWordSplit.forEach(element => /[a-z]/i.test(element) ? arrayWord_.push("_") : arrayWord_.push(element));
+
+//Console.log to check the array of letters changed into "_":
+console.log(arrayWord_);
+
+//Display the word with only "_" instead of letters (separated by spaces " "):
+document.getElementById("randomWordGuess").innerHTML = arrayWord_.join(" ");
+
+//Main loop of the game:
+//For each button:
+document.querySelectorAll(".letter").forEach(button => {
+    //Add event on click:
+    button.addEventListener("click", (event) => {
+        //Set the letter guessed on button click:
+        let letterGuessed = button.innerHTML;
+        //Disable the button:
+        event.target.disabled = true;
+        //Console.log to check if the click happened:
+        console.log(letterGuessed);
+        //Array to test with the uniqueWrong array (condition of loosing game);
+        let testArray = uniqueWrong.length;
+        //If a letter is in the word to guess:
+        if (arrayRandomWordSplit.indexOf(letterGuessed) != -1) {
+            //Iterate through the word to guess:
+            for (let i=0; i<arrayRandomWordSplit.length; i++) {
+                //Replace the "_" by the letter:
+                if (letterGuessed == arrayRandomWordSplit[i]) {
+                    arrayWord_[i] = arrayWord_[i].replace('_', letterGuessed);
+                }
+            }
+            //Write the letter in the HTML:
+            document.getElementById("randomWordGuess").innerHTML = arrayWord_.join(" ");
+            //Push the letter into the array of guessed letters:
+            arrayRightLetters.push(letterGuessed);
+            //Create a no duplicate array to check for the win condition:
+            uniqueRight = [...new Set(arrayRightLetters)];
+            //If both arrays are equal:
+            if (uniqueRight.length + stockNonWordCharacter.length == [...new Set(arrayRandomWordSplit)].length) {
+                document.getElementsByClassName("resultOfGame")[0].innerHTML = "You win !";
+            }
+        }
+        else {
+            //Push the letter into the array of wrong letters:
+            arrayWrongLetters.push(letterGuessed);
+            //Array of condition to loose the game:
+            uniqueWrong = [...new Set(arrayWrongLetters)];
+        }
+        //If both arrays are not equal:
+        if (testArray != uniqueWrong.length) {
+            //Decrement a try from maximum number of tries:
+            numberOfTries--;
+            //If the number of tries equals 0:
+            if (numberOfTries <= 0) {
+                document.getElementsByClassName("resultOfGame")[0].innerHTML = "You loose ! The word was: " + randomWord;
+            }
+        }
+
+        // Draw the part of the body:
+        switch (numberOfTries) {
+            case 9:ground.draw();
+            break;
+            case 8:pole.draw();
+            break;
+            case 7:pole2.draw();
+            break;
+            case 6:rope.draw();
+            break;
+            case 5:head.draw();
+            break;
+            case 4:body.draw();
+            break;
+            case 3:leftArm.draw();
+            break;
+            case 2:rightArm.draw();
+            break;
+            case 1:leftLeg.draw();
+            break;
+            case 0:rightLeg.draw();
+            break;
+        }
+
+        //Some Check to ensure everything is working fine:
+        console.log(uniqueRight);
+        console.log(uniqueWrong.length);
+        console.log(uniqueWrong);
+        console.log(testArray);
+        console.log(numberOfTries);
+    });
+});
+
+//Canvas draw:
+
+//Canvas creation:
+let canvas =  function(){
+    hangman = document.getElementById("hangman");
+    context = hangman.getContext('2d');
+    context.strokeStyle = "black";
+    context.fillStyle = "#fff";
+    context.lineWidth = 3;
+    context.beginPath();
+};
+
+class HangmanParts {
+
+    constructor(x, y, toX, toY, a=false) {
+        this.x = x;
+        this.y = y;
+        this.toX = toX;
+        this.toY = toY;
+        if (a) this.circle=true;
+    }
+
+    draw() {
+        if (this.circle) {
+            context.beginPath();
+            context.arc(this.x, this.y, this.toX, this.toY, Math.PI*2, true);
+            context.strokeStyle = "black";
+
+            context.stroke();
+        }
+        else {
+            context.beginPath();
+            context.moveTo(this.x, this.y);
+            context.lineTo(this.toX, this.toY);
+            context.strokeStyle = "black";
+            context.stroke();
+        }
+    }
+}
+
+canvas();
+
+//Every parts of the body:
+let ground = new HangmanParts(5,145,100,145);
+let pole = new HangmanParts(5,145,5,5);
+let pole2 = new HangmanParts(5,5,75,5);
+let rope = new HangmanParts(75,5,75,15);
+let head = new HangmanParts(75,25,10,0,true);
+let body = new HangmanParts(75,35,75,80);
+let leftArm = new HangmanParts(75,45,55,65);
+let rightArm = new HangmanParts(75,45,95,65);
+let leftLeg = new HangmanParts(75,80,55,100);
+let rightLeg = new HangmanParts(75,80,95,100);
+
+
+//Reload the game:
+document.getElementById("reset").addEventListener("click", () => {
+    document.location.reload();
+});
